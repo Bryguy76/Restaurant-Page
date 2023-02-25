fb703ad72014acbef513cf3e55750b76e8166af1
Bryant Bourgeois <bryant.bourgeois@lmpvwjh25.rcoffice.ringcentral.com>
2023-02-25 13:41:48 -0700

    Webpack config for image loading

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 'use strict';
+import pancake from './assets/pancakes.jpg';
+import room from './assets/dining_room.jpg';
+import burger from './assets/burger.jpg';
+
+const content = document.querySelector('#content');
+const body = document.getElementsByName('body');
+
+const pancakePic = new Image();
+const roomPic = new Image();
+const burgerPic = new Image();
+
+pancakePic.src = pancake;
+roomPic.src = room;
+burgerPic.src = burger;
