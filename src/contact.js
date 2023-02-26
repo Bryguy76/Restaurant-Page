afcf498a8484f19fe8de5d5401e58c49a0c61889
Bryant Bourgeois <bryant.bourgeois@lmpvwjh25.rcoffice.ringcentral.com>
2023-02-26 15:41:26 -0700

    finish contact content

--- src/contact.js
+++ src/contact.js
@@ -1,1 +1,14 @@
-export function contactContent(contentArea) {}
+export function contactContent(contentArea) {
+  const telephone = document.createElement('p');
+  const number = document.createTextNode('(303) 123-1223');
+  telephone.appendChild(number);
+  contentArea.appendChild(telephone);
+  const email = document.createElement('p');
+  const emailAddress = document.createTextNode('help@bourgeois.com');
+  email.appendChild(emailAddress);
+  contentArea.appendChild(email);
+  const address = document.createElement('p');
+  const street = document.createTextNode('123 S Main St, Denver, CO, 80217');
+  address.appendChild(street);
+  contentArea.appendChild(address);
+}
