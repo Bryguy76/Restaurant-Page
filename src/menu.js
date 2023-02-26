e3cbfbb924e2b58fd401bde583e604b3de90a0f2
Bryant Bourgeois <bryant.bourgeois@lmpvwjh25.rcoffice.ringcentral.com>
2023-02-26 15:24:36 -0700

    finish menu content

--- src/menu.js
+++ src/menu.js
@@ -4,5 +4,17 @@ const burgerPic = new Image();
 const pancakePic = new Image();
 pancakePic.src = pancake;
 burgerPic.src = burger;
-
-export function menuContent(contentArea) {}
+pancakePic.classList.add('menuPic');
+burgerPic.classList.add('menuPic');
+export function menuContent(contentArea) {
+  const para1 = document.createElement('p');
+  const pancakePrice = document.createTextNode('pancakes - 12');
+  para1.appendChild(pancakePrice);
+  contentArea.appendChild(para1);
+  contentArea.appendChild(pancakePic);
+  const para2 = document.createElement('p');
+  const burgerPrice = document.createTextNode('burger - 15');
+  para2.appendChild(burgerPrice);
+  contentArea.appendChild(para2);
+  contentArea.appendChild(burgerPic);
+}
